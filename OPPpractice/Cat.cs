b84d175a36f07dd294dbe875891aa7f6d1d863d3
HopeMorehead <splendidwonder@gmail.com>
2017-07-11 13:21:36 -0400

    superHeroclassadded

--- OPPpractice/Cat.cs
+++ OPPpractice/Cat.cs
@@ -7,6 +7,57 @@ using System.Threading.Tasks;
 namespace OPPpractice
 {
     class Cat
-    {
+    { 
+        //Inside of our class, we create  the blueprint for our object.
+        //Theyy should always save as Pascal cat ctrl shift cat
+        //In OOP, there are two types of States:
+        //Fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        
+
+        //properties a short of mix between a field and a method that allow controlled access
+        public string Name
+        {
+
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+        }
+        //we also have behaviore
+        //methods  repeatable/reusable sections of code-action
+        //constructors- specialized methods that are used when instantiating an object
+        public Cat()
+        {
+        // this is an example of a default constructor.A default constructor takes
+       // no arguements/parameter
+        }
+       // allow us to he state of an control the state of an object upon creation.
+      //  always share the name of the class
+      //can have as many constructors as you need as long as they each have a unique signature.
+      //by signature, I mean the parameters in the parentheses after the constructor name.
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry?" + isHungry);
+        }
+
     }
 }
--- OPPpractice/Program.cs
+++ OPPpractice/Program.cs
@@ -10,6 +10,25 @@ namespace OPPpractice
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object, it is called INSTANTIATING(creating a new instance)
+            //An object needs to be instantiated before it can be used
+            //All objects of a specific type are created from the same template -CLASS
+            //All objects from a class will have the same number and type of states, BUT.....
+            //each instance can have it's own unique values.
+            //the format we use to instantiate an objecct looks like this:
+            //ClassName objectName = new ClassName();
+            Cat mittens = new Cat();
+         //   firstCat.Name = "Mittens";
+            //calling constructor in our main method
+            //To create a new object, we use a special method called a CONSTRUCTOR
+            //Thinking about objects in real life, create (either hand-write or type)
+            //Three examples of objects.Your objects should have five states (characteristics)
+            //and five behaviors.
+            Cat secondCat = new Cat("OZ", 3, "Orange and White");
+            secondCat.Name = "Ozzyy";
+            Console.WriteLine(secondCat.Name);
+            Console.WriteLine(secondCat.Age);
+            secondCat.Eat();
         }
     }
 }
